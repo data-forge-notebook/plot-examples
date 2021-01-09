12c142e5cce20d653860c11a23190119e08ae043
Ashley Davis <ashley@codecapers.com.au>
2021-01-09 11:54:27 +1000

    Batch file now uses locally installed ts-node.

--- typescript/run.bat
+++ typescript/run.bat
@@ -1,12 +1,12 @@
-call ts-node example-1.ts
-call ts-node example-2.ts
-call ts-node example-3.ts
-call ts-node example-4.ts
-call ts-node example-5.ts
-call ts-node example-6.ts
-call ts-node example-7.ts
-call ts-node example-8.ts
-call ts-node example-9.ts
-call ts-node example-10.ts
-call ts-node example-11.ts
-call ts-node example-12.ts
+call npx ts-node example-1.ts
+call npx ts-node example-2.ts
+call npx ts-node example-3.ts
+call npx ts-node example-4.ts
+call npx ts-node example-5.ts
+call npx ts-node example-6.ts
+call npx ts-node example-7.ts
+call npx ts-node example-8.ts
+call npx ts-node example-9.ts
+call npx ts-node example-10.ts
+call npx ts-node example-11.ts
+call npx ts-node example-12.ts
